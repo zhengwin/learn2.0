48bc5a51df43c57c953dd492eee4c37986a77c07
Unknown <m3zhen@uwaterloo.ca>
2018-03-04 21:12:13 -0500

    yolo

--- app.js
+++ app.js
@@ -4,12 +4,33 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'); 
+ 
+mongoose.connect('mongodb://localhost/learn-app', { 
+}) 
+  .then(() => console.log('mongodb connected...')) 
+  .catch(err => console.log(err)); 
 
-mongoose.connect('mongodb://localhost/learn-app', {
-})
-  .then(() => console.log('mongodb connected...'))
-  .catch(err => console.log(err));
+//SCHEMA SETUP
+var studentSchema = new mongoose.Schema({
+  first_name: String,
+  last_name: String
+});
+
+var Student = mongoose.model("student", studentSchema);
+
+Student.create(
+{
+  first_name: "Michael",
+  last_name:"Zhen"
+}, function(err, student){
+  if(err){
+    console.log(err);
+  } else {
+    console.log("created student:");
+    console.log(student);
+  }
+});
 
 const index = require('./routes/index');
 const users = require('./routes/users');
@@ -30,6 +51,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
